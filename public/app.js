b6df0d1fce51e6dad3787f4450f07d917d3b61a1
Iamneo <techadmin@iamneo.ai>
2023-09-03 17:44:04 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,64 +1,68 @@
-// Initial game state
-let cells = ['', '', '', '', '', '', '', '', ''];
+// Variables to keep track of the current player (X or O) and whether the game is over.
 let currentPlayer = 'X';
-let result = document.querySelector('.result');
-let btns = document.querySelectorAll('.btn');
-let conditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameWon = false;
 
-// Function to handle player moves
-const ticTacToe = (element, index) => {
-    // Your game logic here
+// Function to handle a player's move.
+function makeMove(button) {
+    const index = parseInt(button.id);
+    
+    // Check if the cell is empty and the game is not over.
+    if (!gameBoard[index] && !gameWon) {
+        gameBoard[index] = currentPlayer;
+        button.innerText = currentPlayer;
+        
+        // Check for a win after every move.
+        if (checkWin()) {
+            document.getElementById('result').innerText = `Player ${currentPlayer} wins!`;
+            gameWon = true;
+        } else if (!gameBoard.includes('')) {
+            document.getElementById('result').innerText = "It's a draw!";
+            gameWon = true;
+        } else {
+            currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+            document.getElementById('result').innerText = `Player ${currentPlayer}'s Turn`;
+        }
+    }
+}
 
-    /*
-    **Part 1: Winning Conditions (Add your code here)**
+// Function to check for a win.
+function checkWin() {
+    const winConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]              // Diagonals
+    ];
+    
+    for (const condition of winConditions) {
+        const [a, b, c] = condition;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+            return true;
+        }
+    }
+    
+    return false;
+}
 
-    1. Implement the logic to check for winning conditions using the 'conditions' array.
-    2. Display a winning message in the 'result' element when a player wins.
-    3. Disable all buttons after a win.
-    */
+// Function to reset the game.
+function resetGame() {
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    gameWon = false;
+    currentPlayer = 'X';
+    document.querySelectorAll('.btn').forEach(button => {
+        button.innerText = '';
+        button.disabled = false;
+    });
+    document.getElementById('result').innerText = `Player ${currentPlayer}'s Turn`;
+}
 
-    // Your code to update the game state and check for a win
-    // ...
-
-    // Your code to display the current player's turn
-    // ...
-
-    // Your code to handle button and cell interactions
-    // ...
-};
-
-    /*
-    **Part 2: Reset Function (Add your code here)**
-
-    1. Implement a new function that resets the game to its initial state.
-    2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
-    3. Update the 'result' element to indicate the current player's turn.
-    4. Re-enable all buttons for a new game.
-    */
-
-// Function to reset the game
-const resetGame = () => {
-    // Your code to reset the game state
-    // ...
-
-    // Your code to update the 'result' element
-    // ...
-
-    // Your code to re-enable buttons
-    // ...
-};
-
-btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => ticTacToe(btn, i));
+// Add event listeners to buttons.
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', function() {
+        makeMove(this);
+        this.disabled = true;
+    });
 });
 
-document.querySelector('#reset').addEventListener('click', resetGame);
+// Initialize the game.
+document.getElementById('result').innerText = `Player ${currentPlayer}'s Turn`;
