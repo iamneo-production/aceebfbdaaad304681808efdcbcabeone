885b0c71d152cb402fa0ca054bbcb63055c4ef2b
Iamneo <techadmin@iamneo.ai>
2023-09-03 18:52:27 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,68 +1,79 @@
-// Variables to keep track of the current player (X or O) and whether the game is over.
+// Initial game state
+let cells = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
-let gameBoard = ['', '', '', '', '', '', '', '', ''];
-let gameWon = false;
+let result = document.querySelector('.result');
+let btns = document.querySelectorAll('.btn');
+let conditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-// Function to handle a player's move.
-function makeMove(button) {
-    const index = parseInt(button.id);
-    
-    // Check if the cell is empty and the game is not over.
-    if (!gameBoard[index] && !gameWon) {
-        gameBoard[index] = currentPlayer;
-        button.innerText = currentPlayer;
-        
-        // Check for a win after every move.
-        if (checkWin()) {
-            document.getElementById('result').innerText = `Player ${currentPlayer} wins!`;
-            gameWon = true;
-        } else if (!gameBoard.includes('')) {
-            document.getElementById('result').innerText = "It's a draw!";
-            gameWon = true;
+// Function to handle player moves
+const ticTacToe = (element, index) => {
+    // Check if the cell is empty and the game is ongoing
+    if (cells[index] === '' && !isGameWon()) {
+        cells[index] = currentPlayer;
+        element.innerText = currentPlayer;
+
+        // Check for a win
+        if (isGameWon()) {
+            result.innerText = `Player ${currentPlayer} wins!`;
+            disableAllButtons();
+        } else if (cells.every(cell => cell !== '')) {
+            result.innerText = "It's a draw!";
         } else {
-            currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
-            document.getElementById('result').innerText = `Player ${currentPlayer}'s Turn`;
+            // Switch to the other player's turn
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            result.innerText = `Player ${currentPlayer}'s Turn`;
         }
     }
-}
+};
 
-// Function to check for a win.
-function checkWin() {
-    const winConditions = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-        [0, 4, 8], [2, 4, 6]              // Diagonals
-    ];
-    
-    for (const condition of winConditions) {
+// Function to check for a win
+const isGameWon = () => {
+    for (const condition of conditions) {
         const [a, b, c] = condition;
-        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+        if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
             return true;
         }
     }
-    
     return false;
-}
+};
 
-// Function to reset the game.
-function resetGame() {
-    gameBoard = ['', '', '', '', '', '', '', '', ''];
-    gameWon = false;
+// Function to disable all buttons
+const disableAllButtons = () => {
+    btns.forEach(btn => (btn.disabled = true));
+};
+
+// Function to reset the game
+const resetGame = () => {
+    // Reset game state
+    cells = ['', '', '', '', '', '', '', '', ''];
     currentPlayer = 'X';
-    document.querySelectorAll('.btn').forEach(button => {
-        button.innerText = '';
-        button.disabled = false;
-    });
-    document.getElementById('result').innerText = `Player ${currentPlayer}'s Turn`;
-}
 
-// Add event listeners to buttons.
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', function() {
-        makeMove(this);
-        this.disabled = true;
+    // Clear the board
+    btns.forEach(btn => {
+        btn.innerText = '';
+        btn.disabled = false;
     });
+
+    // Reset result message
+    result.innerText = `Player ${currentPlayer}'s Turn`;
+};
+
+// Add click event listeners to buttons
+btns.forEach((btn, i) => {
+    btn.addEventListener('click', () => ticTacToe(btn, i));
 });
 
-// Initialize the game.
-document.getElementById('result').innerText = `Player ${currentPlayer}'s Turn`;
+// Add click event listener to the reset button
+document.querySelector('#reset').addEventListener('click', resetGame);
+
+// Initialize the game
+result.innerText = `Player ${currentPlayer}'s Turn`;
